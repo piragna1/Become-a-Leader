55a927221541cd65703c6034c67ee76bd4c23ed5
LuchoDMD <lucio.mdq21@gmail.com>
2024-10-18 20:11:40 -0300

    Agregando Carpetas

---
+++ src/app/components/proof/proof.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proof',
+  standalone: true,
+  imports: [],
+  templateUrl: './proof.component.html',
+  styleUrl: './proof.component.css'
+})
+export class ProofComponent {
+
+}
