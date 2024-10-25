104f92933b65a560228e495c209c6de01875575b
piragna1 <gonzalovarelaalagna@gmail.com>
2024-10-25 20:14:11 -0300

    creacion carpeta "componentes"
    contenido: componente 'curar-equipo' para simular la dinamica dentro del centro pokemon.

---
+++ src/app/componentes/curar-equipo/curar-equipo.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DialogosComponent } from '../../../shared_module/dialogos/dialogos.component';
+import { Pokemon } from '../../types/pokemon';
+
+@Component({
+  selector: 'app-curar-equipo',
+  standalone: true,
+  imports: [DialogosComponent, CommonModule],
+  templateUrl: './curar-equipo.component.html',
+  styleUrl: './curar-equipo.component.css'
+})
+export class CurarEquipoComponent {
+  mostrarDialogo:boolean=true;
+  curacionRealizada:boolean = false;
+  equipo:Pokemon[]=[];
+  textoDialogo:string="Quieres curar a tus pokemon?";
+  opciones:string[]=['Curar','Volver'];
+  curarPokemons(){
+    for (let index = 0; index < this.equipo.length; index++) {
+      this.restaurarHp(this.equipo[index]);
+    }
+    this.curacionRealizada = true;
+  }
+  restaurarHp(pokemon:Pokemon){
+    pokemon.hp=pokemon.maxHp;
+  }
+  recibirOpcionElegida(input:string){
+    if(input ===this.opciones[0]){
+      this.curarPokemons();
+      this.textoDialogo='Has curado a tus pokemon.';
+    }
+    else if (input ===this.opciones[1]){
+      //volver
+    }
+  }
+}
